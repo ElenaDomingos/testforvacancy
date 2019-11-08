e8713a9593030b2123da69ef9bcad3cc1a1c3db7
Elena Domingos <elena-lezhneva@mail.ru>
2019-11-08 19:46:59 -0300

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $("#nationality").selecter({
+        customClass: 'select',
+        label: "American"
+
+
+
+    });
+
+    $("#day").selecter({
+        customClass: 'select-date',
+        label: "Day"
+
+
+
+    });
+
+    $("#month").selecter({
+        customClass: 'select-month',
+        label: "Month"
+
+
+
+    });
+
+    $("#year").selecter({
+        customClass: 'select-date',
+        label: "Year"
+
+
+
+    });
+
+     
+});
+
